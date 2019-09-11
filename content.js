b6001adbf6ea79874bf31036e94fa9238069a015
SiddharthKureel <joykureel@gmail.com>
2019-09-11 22:59:50 +1200

    div, span localName bug

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 document.querySelector('body').addEventListener('click', (event) => {
-    if ((event.target.localName === 'button') || (event.target.localName === 'a')){
+    if ((event.target.localName === 'button') || (event.target.localName === 'a')) {
         let occurrance = {
             type: event.target.localName,
             timeStamp: new Date(),
@@ -9,9 +9,9 @@ document.querySelector('body').addEventListener('click', (event) => {
         }
         chrome.runtime.sendMessage(occurrance, function (response) {
         });
-    } else if (((event.target.localName === 'div') || (event.target.localName === 'span')) && (event.target.childElementCount===0)){
+    } else if (((event.target.localName === 'div') || (event.target.localName === 'span')) && (event.target.childElementCount === 0)) {
         let occurrance = {
-            type: event.target.innerText,
+            type: event.target.localName,
             timeStamp: new Date(),
             value: event.target.innerText,
             location: window.location.href,
@@ -19,7 +19,7 @@ document.querySelector('body').addEventListener('click', (event) => {
         }
         chrome.runtime.sendMessage(occurrance, function (response) {
         });
-    }else if (event.target.localName === 'input') {
+    } else if (event.target.localName === 'input') {
         if (event.target.type === 'password') {
             document.getElementById(event.target.id).addEventListener('change', function (e) {
                 let occurrance = {
@@ -32,19 +32,19 @@ document.querySelector('body').addEventListener('click', (event) => {
                 chrome.runtime.sendMessage(occurrance, function (response) {
                 });
             });
-        } else if (event.target.id){  
-                document.getElementById(event.target.id).addEventListener('change', function (e) {
-              
-                    let occurrance = {
-                        type: e.target.id,
-                        timeStamp: new Date(),
-                        value: e.target.value,
-                        location: window.location.href,
-                        event: 'input',
-                    }
-                    chrome.runtime.sendMessage(occurrance, function (response) {
-                    });
+        } else if (event.target.id) {
+            document.getElementById(event.target.id).addEventListener('change', function (e) {
+
+                let occurrance = {
+                    type: e.target.id,
+                    timeStamp: new Date(),
+                    value: e.target.value,
+                    location: window.location.href,
+                    event: 'input',
+                }
+                chrome.runtime.sendMessage(occurrance, function (response) {
                 });
+            });
         } else {
             document.querySelector(`input[name=${event.target.name}]`).addEventListener('change', function (e) {
                 let occurrance = {
@@ -57,9 +57,9 @@ document.querySelector('body').addEventListener('click', (event) => {
                 chrome.runtime.sendMessage(occurrance, function (response) {
                 });
             })
-        } 
+        }
     } else if (event.target.childElementCount < 1) {
-        if(event.target.localName==='img'){
+        if (event.target.localName === 'img') {
             let occurrance = {
                 type: event.target.localName,
                 timeStamp: new Date(),
@@ -69,7 +69,7 @@ document.querySelector('body').addEventListener('click', (event) => {
             }
             chrome.runtime.sendMessage(occurrance, function (response) {
             });
-        }else{
+        } else {
             let occurrance = {
                 type: event.target.localName,
                 timeStamp: new Date(),
@@ -98,7 +98,7 @@ document.querySelector('body').addEventListener('keyup', (event) => {
                     chrome.runtime.sendMessage(occurrance, function (response) {
                     });
                 });
-            } else if(event.target.id){
+            } else if (event.target.id) {
                 document.getElementById(event.target.id).addEventListener('change', function (e) {
                     let occurrance = {
                         type: e.target.id,

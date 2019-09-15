c4c6f8c9b34d9a5b7d7148a90511c5c8ca23cf9c
SiddharthKureel <joykureel@gmail.com>
2019-09-16 01:03:02 +1200

    v2.3.4 link functionality optimized

--- content.js
+++ content.js
@@ -5,18 +5,21 @@ document.querySelector('body').addEventListener('click', (event) => {
     if (id.name !== '') {
         hierarchy = id.name;
     } 
-    if ((event.target.ownerDocument.activeElement.localName === 'a') || ((event.target.ownerDocument.activeElement.localName === 'paper-item')) && (event.target.childElementCount >= 0)) {
-        let occurrance = {
-            type: 'a',
-            timeStamp: new Date(),
-            frame: hierarchy,
-            value: event.target.ownerDocument.activeElement.href,
-            location: window.location.href,
-            event: 'click',
+    event.path.forEach((element) => {
+        if (element.localName === 'a') {
+            let occurrance = {
+                type: 'a',
+                timeStamp: new Date(),
+                frame: hierarchy,
+                value: element.href,
+                location: window.location.href,
+                event: 'click',
+            }
+            chrome.runtime.sendMessage(occurrance, function (response) {
+            });
         }
-        chrome.runtime.sendMessage(occurrance, function (response) {
-        });
-    } else if (((event.target.localName === 'button') || (event.target.localName === 'div') || (event.target.localName === 'span')) && (event.target.childElementCount === 0)) {
+    })
+    if ((event.target.localName === 'button') && (event.target.childElementCount === 0)) {
         let occurrance = {
             type: event.target.localName,
             timeStamp: new Date(),
@@ -28,17 +31,6 @@ document.querySelector('body').addEventListener('click', (event) => {
         chrome.runtime.sendMessage(occurrance, function (response) {
         });
     }
-    //  else if (((event.target.localName === 'div') || (event.target.localName === 'span')) && (event.target.childElementCount === 0)) {
-    //     let occurrance = {
-    //         type: event.target.localName,
-    //         timeStamp: new Date(),
-    //         value: event.target.innerText,
-    //         location: window.location.href,
-    //         event: 'click',
-    //     }
-    //     chrome.runtime.sendMessage(occurrance, function (response) {
-    //     });
-    // }
      else if (event.target.localName === 'input') {
         if (event.target.type === 'password') {
             document.getElementById(event.target.id).addEventListener('change', function (e) {
@@ -81,17 +73,6 @@ document.querySelector('body').addEventListener('click', (event) => {
                 });
             })
         }
-    } else if (event.target.childElementCount < 1) {
-        let occurrance = {
-            type: event.target.localName,
-            timeStamp: new Date(),
-            frame:hierarchy,
-            value: event.target.innerText,
-            location: window.location.href,
-            event: 'click',
-        }
-        chrome.runtime.sendMessage(occurrance, function (response) {
-        });
     }
 })
 //when user press tab key
